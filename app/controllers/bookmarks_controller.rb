10102deed3855715cbae1705b76ba79663f7b13f
Edgar <x109justice@gmail.com>
2015-01-12 19:07:04 -0600

    update config files

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,8 +8,10 @@ class BookmarksController < ApplicationController
     #doc = Pismo::Document.new(bookmark_params[:link])
     #require 'open-uri'
     #require 'Nokogiri'
-    doc = Nokogiri::HTML(open(bookmark_params[:link]).read)
-    
+    doc = Nokogiri::HTML(open(bookmark_params[:link]).read) do |config|
+      config.strict.nonet
+    end
+
     @bookmark = current_user.bookmarks.build(bookmark_params)
     @bookmark.title = doc.title.to_s
     
@@ -19,8 +21,8 @@ class BookmarksController < ApplicationController
     else
       #redirect_to root_url
       @feed_items = [] # this line is need it case of link is empty
-      redirect_to root_url
-      #render 'static_pages/home'
+      #redirect_to root_url
+      render 'static_pages/home'
     end
   end
 
